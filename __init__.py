4e13033dc1399bbe7436583617981de7c26a611a
root <root@ludovic>
2019-05-25 12:26:57 +0200

    Added skill code

--- __init__.py
+++ __init__.py
@@ -1,5 +1,15 @@
+import json, requests, urllib
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.util.log import getLogger
 
+baseurl = "http://api-ratp.pierre-grimaud.fr/v3/schedules/"
+linetype = "bus"
+line = "72"
+station = "Lamballe-Ankara"
+destination = "R"
+introText = "The next passage is"
+
+LOGGER = getLogger(__name__)
 
 class Ratp(MycroftSkill):
     def __init__(self):
@@ -7,7 +17,18 @@ class Ratp(MycroftSkill):
 
     @intent_file_handler('ratp.intent')
     def handle_ratp(self, message):
-        self.speak_dialog('ratp')
+        LOGGER.info("here")
+        url = baseurl + linetype + "/" + line + "/" + urllib.parse.quote(station) + "/" + destination
+        LOGGER.info("URL: " + url)
+        resp = requests.get(url=url)
+        LOGGER.info("Result: " + resp.text)
+        data = json.loads(resp.text)
+        str = introText
+
+        for dest in data["result"]["schedules"]: 
+            str+= " " + dest["message"].replace("mn", "minutes").replace("A l'approche", "coming")
+            str+= " " + dest["destination"]
+        self.speak(str)
 
 
 def create_skill():
